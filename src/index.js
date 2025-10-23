ed2d2fa01a9074eee7fb05495acce5d660ee9795
benaddy5555 <lahcenbenaddy5555@gmail.com>
2025-10-23 19:23:07 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // si tu veux ajouter un style global
 
-// Point d'entrée principal de l'application
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
